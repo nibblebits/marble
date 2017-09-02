c8f65c2da878c8434560ae7cfe7d13c0e1671b46
= <=>
2017-09-02 10:26:27 +0100

    backup

---
+++ include/negnode.h
@@ -0,0 +1,12 @@
+#ifndef NEGNODE_H
+#define NEGNODE_H
+
+#include "einode.h"
+class NegNode : public ExpressionInterpretableNode
+{
+    public:
+        NegNode();
+        virtual ~NegNode();
+        
+};
+#endif
