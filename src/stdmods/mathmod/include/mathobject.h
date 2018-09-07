892098f69684f31171ae0a225cd263ce3a8ac159
Daniel McCarthy <dansblackberry2@hotmail.co.uk>
2018-09-07 19:10:33 +0100

    Implemented more math functions

--- src/stdmods/mathmod/include/mathobject.h
+++ src/stdmods/mathmod/include/mathobject.h
@@ -21,5 +21,26 @@ class MathObject : public Object
     static void Math_abs(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
     static void Math_acos(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
     static void Math_acosh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_tan(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_asin(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_atan(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_atan2(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_cosh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_sinh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_tanh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_asinh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_atanh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_exp(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_ldexp(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_log(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_log10(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_exp2(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_ilogb(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_log1p(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_log2(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_logb(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_scalbn(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+    static void Math_scalbln(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope);
+
 };
 #endif
--- src/stdmods/mathmod/src/mathobject.cpp
+++ src/stdmods/mathmod/src/mathobject.cpp
@@ -1,6 +1,7 @@
 #include "../include/mathobject.h"
 #include "modulesystem.h"
 #include <math.h>
+#include <tgmath.h>
 
 MathObject::MathObject(Class *c) : Object(c)
 {
@@ -65,35 +66,289 @@ void MathObject::registerClass(ModuleSystem *moduleSystem)
      * function acos(number x) : number
      */
     c->registerFunction("acosh", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_acosh);
+
+    /**
+     * @class Math
+     * 
+     * Returns the tangent of an angle of x radians.
+     * function tan(number x) : number
+     */
+    c->registerFunction("tan", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_tan);
+
+    /**
+     * @class Math
+     * 
+     * Returns the principal value of the arc sine of x, expressed in radians.
+     * function asin(number x) : number
+     */
+    c->registerFunction("asin", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_asin);
+
+    /**
+     * @class Math
+     * 
+     * Returns the principal value of the arc tangent of x, expressed in radians.
+     * In trigonometrics, arc tangent is the inverse operation of tangent.
+     * function atan(number x) : number
+     */
+    c->registerFunction("atan", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_atan);
+
+    /**
+     * @class Math
+     * 
+        Returns the principal value of the arc tangent of y/x, expressed in radians.
+        To compute the value, the function takes into account the sign of both arguments in order to determine the quadrant.
+        In C++, this function is overloaded in <valarray> (see valarray atan2).
+     * function atan2(number y, number x) : number
+     */
+    c->registerFunction("atan2", {VarType::fromString("number"), VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_atan2);
+
+    /**
+     * @class Math
+     * 
+     * Returns the hyperbolic cosine of x.
+     * function cosh(number x) : number
+     */
+    c->registerFunction("cosh", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_cosh);
+
+    /**
+     * @class Math
+     * 
+     * Returns the hyperbolic sine of x.
+     * function sinh(number x) : number
+     */
+    c->registerFunction("sinh", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_sinh);
+
+    /**
+     * @class Math
+     * 
+     * Returns the hyperbolic tangent of x.
+     * function tanh(number x) : number
+     */
+    c->registerFunction("tanh", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_tanh);
+
+    /**
+     * @class Math
+     * 
+     * Returns the area hyperbolic tangent of x.
+     * function asinh(number x) : number
+     */
+    c->registerFunction("asinh", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_asinh);
+
+    /**
+     * @class Math
+     * 
+     * Returns the area hyperbolic sine of x.
+     * function asinh(number x) : number
+     */
+    c->registerFunction("atanh", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_atanh);
+
+    /**
+     * @class Math
+     * Returns the base-e exponential function of x, which is e raised to the power x: ex.
+     * function exp(number x) : number
+     */
+    c->registerFunction("exp", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_exp);
+
+    /**
+     * @class Math
+     * 
+     * Returns the result of multiplying x (the significand) by 2 raised to the power of exp (the exponent).
+     * function ldexp(number x, number exp) : number
+     */
+    c->registerFunction("ldexp", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_ldexp);
+
+    /**
+     * @class Math
+     *  Returns the natural logarithm of x.
+     * function log(number x) : number
+     */
+    c->registerFunction("log", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_log);
+
+    /**
+     * @class Math
+     * Returns the common (base-10) logarithm of x.
+     * function log10(number x) : number
+     */
+    c->registerFunction("log10", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_log10);
+
+    /**
+     * @class Math
+     *  Returns the base-2 exponential function of x, which is 2 raised to the power x: 2x.
+     * function exp2(number x) : number
+     */
+    c->registerFunction("exp2", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_exp2);
+
+    /**
+     * @class Math
+     *  Returns the integral part of the logarithm of |x|, using FLT_RADIX as base for the logarithm.
+     * function ilogb(number x) : number
+     */
+    c->registerFunction("ilogb", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_ilogb);
+
+    /**
+     * @class Math
+     * Returns the natural logarithm of one plus x.
+     * function log1p(number x) : number
+     */
+    c->registerFunction("log1p", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_log1p);
+
+    /**
+     * @class Math
+     * Returns the binary (base-2) logarithm of x.
+     * function log2(number x) : number
+     */
+    c->registerFunction("log1p", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_log2);
+
+    /**
+     * @class Math
+     *  Returns the logarithm of |x|, using FLT_RADIX as base for the logarithm.
+     * function logb(number x) : number
+     */
+    c->registerFunction("logb", {VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_logb);
+
+    /**
+     * @class Math
+     * 
+     * Scales x by FLT_RADIX raised to the power of n, returning the same as:
+     * scalbn(x,n) = x * FLT_RADIXn
+     * Presumably, x and n are the components of a floating-point number in the system; In such a case, this function may be optimized to be more efficient than the theoretical operations to compute the value explicitly.
+     * function scalbn(number x, number n) : number
+     */
+    c->registerFunction("scalbn", {VarType::fromString("number"), VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_scalbn);
+
+    /**
+     * @class Math
+     * 
+     *  Scales x by FLT_RADIX raised to the power of n, returning the result of computing:
+     *  scalbn(x,n) = x * FLT_RADIXn
+     * 
+     *  Presumably, x and n are the components of a floating-point number in the system; In such a case, this function may be optimized to be more efficient than the theoretical operations to compute the value explicitly.
+     * function scalbln(number x, number n) : number
+     */
+    c->registerFunction("scalbln", {VarType::fromString("number"), VarType::fromString("number")}, VarType::fromString("number"), MathObject::Math_scalbln);
 }
 
-
-void MathObject::Math_cos(Interpreter* interpreter, std::vector<Value> values, Value* return_value, std::shared_ptr<Object> object, Scope* caller_scope)
+void MathObject::Math_cos(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
 {
-    return_value->set((double) cos(values[0].dvalue));
+    return_value->set((double)cos(values[0].dvalue));
 }
 
-void MathObject::Math_sin(Interpreter* interpreter, std::vector<Value> values, Value* return_value, std::shared_ptr<Object> object, Scope* caller_scope)
+void MathObject::Math_sin(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
 {
-    return_value->set((double) sin(values[0].dvalue));
+    return_value->set((double)sin(values[0].dvalue));
 }
 
-void MathObject::Math_abs(Interpreter* interpreter, std::vector<Value> values, Value* return_value, std::shared_ptr<Object> object, Scope* caller_scope)
+void MathObject::Math_abs(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
 {
-    return_value->set((double) abs(values[0].dvalue));
+    return_value->set((double)abs(values[0].dvalue));
 }
 
-void MathObject::Math_acos(Interpreter* interpreter, std::vector<Value> values, Value* return_value, std::shared_ptr<Object> object, Scope* caller_scope)
+void MathObject::Math_acos(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
 {
-    return_value->set((double) acos(values[0].dvalue));
+    return_value->set((double)acos(values[0].dvalue));
 }
 
-
-void MathObject::Math_acosh(Interpreter* interpreter, std::vector<Value> values, Value* return_value, std::shared_ptr<Object> object, Scope* caller_scope)
+void MathObject::Math_acosh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
 {
-    return_value->set((double) acosh(values[0].dvalue));
+    return_value->set((double)acosh(values[0].dvalue));
 }
 
+void MathObject::Math_tan(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)tan(values[0].dvalue));
+}
 
+void MathObject::Math_asin(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)asin(values[0].dvalue));
+}
 
+void MathObject::Math_atan(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)atan(values[0].dvalue));
+}
 
+void MathObject::Math_atan2(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)atan2(values[0].dvalue, values[1].dvalue));
+}
+
+void MathObject::Math_cosh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)cosh(values[0].dvalue));
+}
+
+void MathObject::Math_sinh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)sinh(values[0].dvalue));
+}
+
+void MathObject::Math_tanh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)tanh(values[0].dvalue));
+}
+
+void MathObject::Math_asinh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)asinh(values[0].dvalue));
+}
+
+void MathObject::Math_atanh(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)atanh(values[0].dvalue));
+}
+
+void MathObject::Math_exp(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)exp(values[0].dvalue));
+}
+
+void MathObject::Math_ldexp(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)ldexp(values[0].dvalue, values[1].dvalue));
+}
+
+void MathObject::Math_log(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)log(values[0].dvalue));
+}
+
+void MathObject::Math_log10(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)log10(values[0].dvalue));
+}
+
+void MathObject::Math_exp2(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)exp2(values[0].dvalue));
+}
+
+void MathObject::Math_ilogb(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)ilogb(values[0].dvalue));
+}
+
+void MathObject::Math_log1p(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)log1p(values[0].dvalue));
+}
+
+void MathObject::Math_log2(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)log2(values[0].dvalue));
+}
+
+void MathObject::Math_logb(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)logb(values[0].dvalue));
+}
+
+void MathObject::Math_scalbn(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)scalbn(values[0].dvalue, (int)values[0].dvalue));
+}
+
+void MathObject::Math_scalbln(Interpreter *interpreter, std::vector<Value> values, Value *return_value, std::shared_ptr<Object> object, Scope *caller_scope)
+{
+    return_value->set((double)scalbln(values[0].dvalue, (int)values[0].dvalue));
+}
